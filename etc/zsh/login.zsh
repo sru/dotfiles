606e29464b1dd7597081f4f2212d1b36ea654d0e
Sung Rim Huh <sungrimhuh@gmail.com>
2021-03-14 10:09:17 -0700

    zsh: don't rely on XDG_ variables

--- etc/zsh/login.zsh
+++ etc/zsh/login.zsh
@@ -1,12 +1,12 @@
 # Start SSH agent if it's not already running.
-if ! pgrep -u "${USER}" ssh-agent > /dev/null; then
-  ssh-agent -t 7200 -a "${XDG_RUNTIME_DIR}/ssh-agent.socket" > "${XDG_RUNTIME_DIR}/ssh-agent.env"
+if ! pgrep -u "${USER}" ssh-agent >/dev/null; then
+  ssh-agent -t 7200 -a "/tmp/ssh-agent-$(id -u).socket" >"/tmp/ssh-agent-$(id -u).env"
 fi
-if [[ ! "${SSH_AUTH_SOCK}" ]]; then
-  source "${XDG_RUNTIME_DIR}/ssh-agent.env" > /dev/null
+if [[ ! -v SSH_AUTH_SOCK ]]; then
+  source "/tmp/ssh-agent-$(id -u).env" >/dev/null
 fi
 
 # Start X on login.
-if [[ ! "${DISPLAY}" && "${XDG_VTNR}" -eq 1 ]]; then
-  exec sx
+if [[ ! "${DISPLAY}" ]]; then
+  sx
 fi
