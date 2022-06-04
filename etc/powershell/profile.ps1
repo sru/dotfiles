bc76203118f307566a1045de1766b626961a6466
Sung Rim Huh <sungrimhuh@gmail.com>
2022-06-04 16:03:03 -0700

    use less colorful PowerShell prompt

--- etc/powershell/profile.ps1
+++ etc/powershell/profile.ps1
@@ -5,12 +5,24 @@ if ($host.Name -eq 'ConsoleHost') {
   $params = @{
     EditMode = 'Emacs'
     Colors = @{
-      Command = "$([char]0x1b)[33m"
-      Error = "$([char]0x1b)[31m"
-      Member = "$([char]0x1b)[34m"
-      Number = "$([char]0x1b)[34m"
-      String = "$([char]0x1b)[34m"
-      Variable = "$([char]0x1b)[32m"
+      Command = "$([char]0x1b)[30m"
+      Comment = "$([char]0x1b)[32m"
+      ContinuationPrompt = "$([char]0x1b)[30m"
+      Default = "$([char]0x1b)[30m"
+      Emphasis = "$([char]0x1b)[30;103m"
+      Error = "$([char]0x1b)[30;101m"
+      InlinePrediction = "$([char]0x1b)[90m"
+      Keyword = "$([char]0x1b)[30m"
+      ListPrediction = "$([char]0x1b)[30m"
+      ListPredictionSelected = "$([char]0x1b)[30;100m"
+      Member = "$([char]0x1b)[30m"
+      Number = "$([char]0x1b)[30m"
+      Operator = "$([char]0x1b)[30m"
+      Parameter = "$([char]0x1b)[30m"
+      Selection = "$([char]0x1b)[30;100m"
+      String = "$([char]0x1b)[30m"
+      Type = "$([char]0x1b)[30m"
+      Variable = "$([char]0x1b)[30m"
     }
   }
   Set-PSReadLineOption @params
