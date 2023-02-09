6be93074695de529f29fcca873b108974e0e186a
Sung Rim Huh <sungrimhuh@gmail.com>
2023-02-09 09:36:54 -0800

    remove vim.opt usage and add formatoptions override

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -3,94 +3,95 @@ local autocmd_id = vim.api.nvim_create_augroup('vimrc', {
   clear = true
 })
 
+local hide_trailing_space = function()
+  local new_listchars = {}
+  for listchar in vim.o.listchars:gmatch('([^,]+)') do
+    if listchar ~= 'trail:-' then
+      table.insert(new_listchars, listchar)
+    end
+  end
+  vim.o.listchars = table.concat(new_listchars, ',')
+end
+
+local show_trailing_space = function()
+  for listchar in vim.o.listchars:gmatch('([^,]+)') do
+    if listchar == 'trail:-' then
+      return
+    end
+  end
+  vim.o.listchars = vim.o.listchars .. ',trail:-'
+end
+
 local strip_trailing_whitespaces = function()
   local saved_view = vim.fn.winsaveview()
   vim.cmd('silent! keepjumps keeppatterns %s/\\s\\+$//')
   vim.fn.winrestview(saved_view)
 end
 
--- Follow XDG.
-local data_path
-if vim.env.XDG_STATE_HOME ~= nil then
-  data_path = vim.env.XDG_STATE_HOME .. '/nvim'
-elseif vim.fn.has('win32') ~= 1 then
-  data_path = vim.env.HOME .. '/.local/state/nvim'
-elseif vim.env.LOCALAPPDATA ~= nil then
-  data_path = vim.env.LOCALAPPDATA .. '/nvim-data'
-else
-  data_path = vim.env.HOME .. '/AppData/Local/nvim-data'
-end
+vim.cmd.colorscheme('seon')
+vim.cmd('filetype indent off')
 
-local swap_path = data_path .. '/swap'
-local undo_path = data_path .. '/undo'
+vim.g.netrw_home = vim.fn.stdpath('state')
 
-vim.cmd [[
-  colorscheme seon
-
-  syntax on
-  filetype on
-  filetype indent off
-  filetype plugin on
-]]
-
-vim.g.netrw_home = data_path
-
-vim.opt.autoindent = true
-vim.opt.autoread = true
-vim.opt.backspace = {'indent', 'eol', 'start'}
-vim.opt.complete:remove('i')
-vim.opt.completeopt = {'menu', 'menuone', 'longest'}
-vim.opt.confirm = false
-vim.opt.cpoptions:remove('_')
-vim.opt.directory = {swap_path .. '//', '.'}
-vim.opt.encoding = 'utf-8'
-vim.opt.expandtab = true
-vim.opt.fileformats = {'unix', 'dos'}
-vim.opt.hidden = true
-vim.opt.history = 10000
-vim.opt.hlsearch = false
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.joinspaces = false
-vim.opt.laststatus = 0
-vim.opt.lazyredraw = true
-vim.opt.list = true
-vim.opt.listchars = {tab = '>-', trail = '-'}
-vim.opt.modeline = false
-vim.opt.number = false
-vim.opt.ruler = false
-vim.opt.shadafile = data_path .. '/shada'
-vim.opt.shiftwidth = 2
-vim.opt.shortmess = 'atI'
-vim.opt.showcmd = true
-vim.opt.showmatch = false
-vim.opt.showmode = true
-vim.opt.smartcase = true
-vim.opt.softtabstop = 2
-vim.opt.statusline = "%{repeat('-',winwidth(0))}"
-vim.opt.textwidth = 80
-vim.opt.undodir = undo_path
-vim.opt.undofile = true
-vim.opt.undolevels = 1000
-vim.opt.wildmenu = true
-vim.opt.wildmode = 'list:longest'
+vim.o.autoindent = true
+vim.o.autoread = true
+vim.o.backspace = 'indent,eol,start'
+vim.o.completeopt = 'menu,menuone,longest'
+vim.o.confirm = false
+vim.o.cpoptions = vim.o.cpoptions:gsub('_', '')
+vim.o.encoding = 'utf-8'
+vim.o.expandtab = true
+vim.o.fileformats = 'unix,dos'
+vim.o.formatoptions = 'ro/qj'
+vim.o.hidden = true
+vim.o.history = 10000
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.joinspaces = false
+vim.o.laststatus = 0
+vim.o.lazyredraw = true
+vim.o.list = true
+vim.o.listchars = 'tab:>-,trail:-'
+vim.o.modeline = false
+vim.o.number = false
+vim.o.ruler = false
+vim.o.shiftwidth = 2
+vim.o.shortmess = 'atI'
+vim.o.showcmd = true
+vim.o.showmatch = false
+vim.o.showmode = true
+vim.o.smartcase = true
+vim.o.softtabstop = 2
+vim.o.statusline = "%{repeat('-',winwidth(0))}"
+vim.o.textwidth = 80
+vim.o.undofile = true
+vim.o.undolevels = 1000
+vim.o.wildmenu = true
+vim.o.wildmode = 'list:longest'
 
 vim.api.nvim_create_autocmd('InsertEnter', {
   group = autocmd_id,
   desc = 'Hide trailing whitespace when entering insert mode.',
-  callback = function() vim.opt.listchars:remove({trail = '-'}) end,
+  callback = hide_trailing_space,
 })
 
 vim.api.nvim_create_autocmd('InsertLeave', {
   group = autocmd_id,
   desc = 'Show trailing whitespace when leaving insert mode.',
-  callback = function() vim.opt.listchars:append({trail = '-'}) end,
+  callback = show_trailing_space,
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = autocmd_id,
   desc = 'Remove trailing whitespaces when saving.',
-  callback = function() strip_trailing_whitespaces() end,
+  callback = strip_trailing_whitespaces,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = autocmd_id,
+  desc = 'Override formatoptions.',
+  callback = function() vim.o.formatoptions = 'ro/qj' end,
 })
 
 vim.keymap.set('n', 'Y', 'y$')
