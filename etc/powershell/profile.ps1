4feb0195d52a9a6b3954fec1c4f106ba7dd5bd5a
Sung Rim Huh <sungrimhuh@gmail.com>
2024-06-29 11:04:31 -0700

    Add additional color customization for PowerShell

--- etc/powershell/profile.ps1
+++ etc/powershell/profile.ps1
@@ -5,28 +5,65 @@ if ($host.Name -eq 'ConsoleHost') {
   $params = @{
     EditMode = 'Emacs'
     Colors = @{
-      Command = "$([char]0x1b)[30m"
-      Comment = "$([char]0x1b)[32m"
-      ContinuationPrompt = "$([char]0x1b)[30m"
-      Default = "$([char]0x1b)[30m"
-      Emphasis = "$([char]0x1b)[30;103m"
-      Error = "$([char]0x1b)[30;101m"
+      Command = "$([char]0x1b)[39m"
+      Comment = "$([char]0x1b)[39;3m"
+      ContinuationPrompt = "$([char]0x1b)[39m"
+      Default = "$([char]0x1b)[39m"
+      Emphasis = "$([char]0x1b)[39;103m"
+      Error = "$([char]0x1b)[31m"
       InlinePrediction = "$([char]0x1b)[90m"
-      Keyword = "$([char]0x1b)[30m"
-      ListPrediction = "$([char]0x1b)[30m"
-      ListPredictionSelected = "$([char]0x1b)[30;100m"
-      Member = "$([char]0x1b)[30m"
-      Number = "$([char]0x1b)[30m"
-      Operator = "$([char]0x1b)[30m"
-      Parameter = "$([char]0x1b)[30m"
-      Selection = "$([char]0x1b)[30;100m"
-      String = "$([char]0x1b)[30m"
-      Type = "$([char]0x1b)[30m"
-      Variable = "$([char]0x1b)[30m"
+      Keyword = "$([char]0x1b)[39m"
+      ListPrediction = "$([char]0x1b)[39m"
+      ListPredictionSelected = "$([char]0x1b)[39;100m"
+      Member = "$([char]0x1b)[39m"
+      Number = "$([char]0x1b)[39m"
+      Operator = "$([char]0x1b)[39m"
+      Parameter = "$([char]0x1b)[39m"
+      Selection = "$([char]0x1b)[39;100m"
+      String = "$([char]0x1b)[39m"
+      Type = "$([char]0x1b)[39m"
+      Variable = "$([char]0x1b)[39m"
     }
     PredictionSource = 'None'
   }
   Set-PSReadLineOption @params
+
+  $Host.PrivateData.FormatAccentColor = 'Black'
+  $Host.PrivateData.ErrorAccentColor = 'Red'
+  $Host.PrivateData.ErrorForegroundColor = 'Red'
+  $Host.PrivateData.ErrorBackgroundColor = 'White'
+  $Host.PrivateData.WarningForegroundColor = 'Yellow'
+  $Host.PrivateData.WarningBackgroundColor = 'White'
+  $Host.PrivateData.DebugForegroundColor = 'Gray'
+  $Host.PrivateData.DebugBackgroundColor = 'White'
+  $Host.PrivateData.VerboseForegroundColor = 'Gray'
+  $Host.PrivateData.VerboseBackgroundColor = 'White'
+  $Host.PrivateData.ProgressForegroundColor = 'Black'
+  $Host.PrivateData.ProgressBackgroundColor = 'White'
+
+  # PromptForChoice hard-codes color based on background color.
+  $Host.UI.RawUI.ForegroundColor = 'Black'
+  $Host.UI.RawUI.BackgroundColor = 'White'
+
+  # $PSStyle is only available on PowerShell 7.2 or later.
+  if (Test-Path variable:PSStyle) {
+    $PSStyle.Formatting.FormatAccent = "$([char]0x1b)[1m"
+    $PSStyle.Formatting.ErrorAccent = "$([char]0x1b)[31;1m"
+    $PSStyle.Formatting.Error = "$([char]0x1b)[31m"
+    $PSStyle.Formatting.Warning = "$([char]0x1b)[33m"
+    $PSStyle.Formatting.Verbose = "$([char]0x1b)[37m"
+    $PSStyle.Formatting.Debug = "$([char]0x1b)[37m"
+    $PSStyle.Formatting.TableHeader = "$([char]0x1b)[39m"
+    $PSStyle.Formatting.CustomTableHeaderLabel = "$([char]0x1b)[39m"
+    $PSStyle.Formatting.FeedbackName = "$([char]0x1b)[39m"
+    $PSStyle.Formatting.FeedbackText = "$([char]0x1b)[39m"
+    $PSStyle.Formatting.FeedbackAction = "$([char]0x1b)[39m"
+    $PSStyle.Progress.Style = "$([char]0x1b)[39m"
+    $PSStyle.FileInfo.Directory = "$([char]0x1b)[34m"
+    $PSStyle.FileInfo.SymbolicLink = "$([char]0x1b)[36m"
+    $PSStyle.FileInfo.Executable = "$([char]0x1b)[32m"
+    $PSStyle.FileInfo.Extension.Clear()
+  }
 
   Set-Alias g git
 
