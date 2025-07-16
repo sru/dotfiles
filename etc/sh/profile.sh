053540c21d868e0d84f2a7e47f7922e90c705f93
Sung Rim Huh <sungrimhuh@gmail.com>
2025-07-15 20:29:30 -0700

    Use Neovim as default editor

--- etc/sh/profile.sh
+++ etc/sh/profile.sh
@@ -3,8 +3,8 @@ set -a
 eval "$(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)"
 set +a
 
-export EDITOR='/usr/bin/helix'
-export VISUAL='/usr/bin/helix'
+export EDITOR='/usr/bin/nvim'
+export VISUAL='/usr/bin/nvim'
 export PAGER='/usr/bin/less'
 
 # SSH_AUTH_SOCK variable may be set if SSH Agent is forwarded.
