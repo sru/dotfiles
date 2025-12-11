4d55cc106ed5582d25b33d8f700849bde7e65279
Sung Rim  Huh <sungrimhuh@gmail.com>
2025-12-10 19:15:17 -0800

    Add Yazi configuration

--- etc/powershell/profile.ps1
+++ etc/powershell/profile.ps1
@@ -85,6 +85,18 @@ if ($host.Name -eq 'ConsoleHost') {
   Remove-Item Alias:wget -ErrorAction SilentlyContinue
 }
 
+if (Get-Command 'yazi.exe' -ErrorAction SilentlyContinue) {
+  function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+      Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+  }
+}
+
 function Prompt {
   '> '
 }
