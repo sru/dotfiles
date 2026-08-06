e160f8a9ae5166da08b47b422aa1e1d2f9bdeaf1
Sung Rim  Huh <sungrimhuh@gmail.com>
2026-08-05 22:39:56 -0700

    Use mini.nvim

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -58,24 +58,15 @@ require("statusline")
 require("trailing-whitespace")
 
 vim.pack.add({
-  "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/nvim-mini/mini.align",
-  "https://github.com/nvim-mini/mini.surround",
-  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/nvim-mini/mini.nvim",
 })
 
-require("fzf-lua").setup({
-  winopts = {
-    backdrop = 100,
-    preview = {
-      hidden = true,
-    },
-  },
-})
-
+require("mini.ai").setup()
 require("mini.align").setup()
-
+require("mini.operators").setup()
+require("mini.splitjoin").setup()
 require("mini.surround").setup()
 
-require("oil").setup()
-vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+require("mini.bracketed").setup()
+require("mini.files").setup()
+require("mini.pick").setup()
